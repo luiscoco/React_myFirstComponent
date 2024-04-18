55957aabbc85ac4209f1dfa8c64e0581e7d59bc2
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-04-18 09:09:27 +0200

    adding a new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyNewComponent from './components/MyNewComponent';
+
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <div className="App">
+           <MyNewComponent />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/MyNewComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyNewComponent() {
+  return (
+    <div>
+      <h1>Hello, this is my new component!</h1>
+    </div>
+  );
+}
+
+export default MyNewComponent;
